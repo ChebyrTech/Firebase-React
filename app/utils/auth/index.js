3dacfc768e9f86ac6d484155e66db82366ad0607
SvitlanaTimofeyeva <s.timofeyeva@dinamicka.com>
2017-01-23 14:26:15 +0200

    authentification

--- app/utils/auth/index.js
+++
@@ -1,5 +0,0 @@
-export { isAuthenticated } from './selectors';
-export { initAuth } from './auth';
-export { authReducer } from './reducer';
-export { authActions } from './actions';
-export { authSaga, watchSignIn, sagas } from './sagas';
